e6cf446489b13b830bd4b0adc6abc6ce6fe61ff0
arinop12 <your-email@example.com>
2025-01-29 16:45:54 +0530

    Added The Post Functionality

--- Server/index.cjs
+++ Server/index.cjs
@@ -19,17 +19,28 @@ const client = new Client({
 client.connect()
 
 app.post('/todosubmit',async (req,res)=>{
+    const {todo , todotype , tododate , todocolor , tododesc} = req.body;
+    console.log(req.body)
     try {
-        const result = await client.query('INSERT INTO usertodo (todo , todotype , todostatus , tododate , tododesc) VALUES ($1 ,$2 ,$3 ,$4 ,$5) RETURNING *',
-            [todo , todotype , todostatus , tododate , tododesc]
+        const result = await client.query('INSERT INTO todo (todo , todotype , tododate  , tododesc , todocolor) VALUES ($1 ,$2 ,$3 ,$4 ,$5) RETURNING *',
+            [todo , todotype , tododate  , tododesc , todocolor]
         )
         console.log(result.rows)
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.log('There is an error', error)
+    }
+    
+})
+
+app.get('/gettodo' , async (req , res)=>{
+    try {
+        const response = await client.query('SELECT * FROM usertodo')
+        res.json(response.rows)
     } catch (error) {
         console.log('There is an error', error)
     }
 })
-
-
 
 app.listen(PORT , (req , res)=>{
     console.log("Server is Running on" , PORT)
